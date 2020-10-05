a5e14ea5c03a61110047b5729a3a4030f84d91e5
DevroDev <devroshein@gmail.com>
2020-10-06 02:11:29 +0630

    'finished-project'

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Welcome to Giggle
+
+This is the community of Nerds and Meme lovers and first rule of the Giggle is you do not tell about Giggle
+
+Wish you have a grate day!<br>
+Devro
+@endcomponent
